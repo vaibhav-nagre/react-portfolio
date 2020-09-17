db27ef8e6565204c88d575670ed9c521e366bdca
vaibhav <nagre.vaibhav8@gmail.com>
2020-09-17 16:33:46 -0700

    Updates

---
+++ precache-manifest.243f9283f0f4be6d1123b18f31f8ff87.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195a7c2d76a7f6f3e65ed83185104403",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3205f949ea482a5c28df",
+    "url": "/react-portfolio/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "9409e348d6da4e22c67f",
+    "url": "/react-portfolio/static/css/main.653f69f3.chunk.css"
+  },
+  {
+    "revision": "3205f949ea482a5c28df",
+    "url": "/react-portfolio/static/js/2.e892a871.chunk.js"
+  },
+  {
+    "revision": "0001512ff5376eafd7f76369861b7a6e",
+    "url": "/react-portfolio/static/js/2.e892a871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9409e348d6da4e22c67f",
+    "url": "/react-portfolio/static/js/main.b36318f9.chunk.js"
+  },
+  {
+    "revision": "51b4af488cc40aadb4e5",
+    "url": "/react-portfolio/static/js/runtime-main.b84e174f.js"
+  },
+  {
+    "revision": "73ff87778d4bc03f1a7d859835005672",
+    "url": "/react-portfolio/static/media/Profile.73ff8777.jpg"
+  },
+  {
+    "revision": "99697b1727a05028c27d0d353c5b464a",
+    "url": "/react-portfolio/static/media/nagre_resume.99697b17.pdf"
+  },
+  {
+    "revision": "9c1c9d5541583708050b2680033bccda",
+    "url": "/react-portfolio/static/media/seefoodapp.9c1c9d55.jpg"
+  }
+]);
